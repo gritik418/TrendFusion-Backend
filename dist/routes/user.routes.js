d5652dd69dd978385036990153df1775e2579003
Ritik <160255700+gritik418@users.noreply.github.com>
2024-11-10 18:29:41 +0530

    Update user phone number route completed

--- dist/routes/user.routes.js
+++ dist/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user.js";
+import { getUser, updatePhoneNumber } from "../controllers/user.js";
 import authenticate from "../middlewares/authenticate.js";
 const router = Router();
 router.get("/", authenticate, getUser);
+router.patch("/edit/phonenumber", authenticate, updatePhoneNumber);
 export default router;
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -29,3 +29,43 @@ export const getUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const updatePhoneNumber = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+    const phoneNumber: string | null = req.body.phoneNumber;
+
+    if (!phoneNumber) {
+      return res.status(400).json({
+        success: false,
+        message: "Phone Number is required.",
+      });
+    }
+
+    if (phoneNumber.length !== 10 || !phoneNumber.match(/^\d+$/)) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter a valid Phone Number.",
+      });
+    }
+
+    if (!userId) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found.",
+      });
+    }
+
+    await User.findByIdAndUpdate(userId, { $set: { phoneNumber } });
+
+    return res.status(200).json({
+      success: true,
+      message: "Phone Number Updated!",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Error.",
+    });
+  }
+};
